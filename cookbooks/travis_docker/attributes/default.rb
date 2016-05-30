531a4ecc099c07e5baec2196e15bbd4f77cd8193
Lyoness <carmen.andoh@gmail.com>
2016-05-30 13:37:16 +0200

    fixed ref headers

--- cookbooks/travis_docker/attributes/default.rb
+++ cookbooks/travis_docker/attributes/default.rb
@@ -1,8 +1,4 @@
 default['travis_docker']['version'] = '1.10.0~trusty'
 default['travis_docker']['users'] = %w(travis)
 default['travis_docker']['compose']['url'] = 'https://github.com/docker/compose/releases/download/1.7.1/docker-compose-Linux-x86_64'
-<<<<<<< HEAD
 default['travis_docker']['compose']['sha256sum'] = '3166bb74bc648e68c3154bc704fddf6bccf59f03a0c90fc48aefac034535e4ae'
-=======
-default['travis_docker']['compose']['sha256sum'] = 'd5fca08d54f59649b93b66a781b22998955f2bd701244fcfd650c00daa9e948c'
->>>>>>> a6ace4bed4a2ef67b165be77366d88b19affa8fa
